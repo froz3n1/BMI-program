5a4a66296d3926ddc5ac540a8c31036be957b16d
froz3n1 <76907880+froz3n1@users.noreply.github.com>
2022-03-31 04:44:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Enter your weight in kg")
+weight=float(input())
+print("Enter your height in m")
+height=float(input())
+bmi=int(weight/height**2)
+print("Your BMI is"" "+str(bmi))
+
+
+
+
+
+
