1d6e180baa04153b192f8a6da293099b0a027caf
froz3n1 <76907880+froz3n1@users.noreply.github.com>
2022-04-01 07:34:29 +0000

    Used if , nested if and elif statement  to interpret the BMI based on the BMI value.

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ print("Enter your weight in kg")
 weight=float(input())
 print("Enter your height in m")
 height=float(input())
-bmi=int(weight/height**2)
-print("Your BMI is"" "+str(bmi))
+bmi=round(weight/height**2)
+if bmi<35:
+  if bmi>=30:
+    print(f"Your BMI is {bmi} you are obese")
+  elif bmi>=25:
+     print(f"Your BMI is {bmi} you are slightly overweight")
+  elif bmi<=18.5:
+    print(f"Your BMI is {bmi} you are underweight")
+  else:
+    print(f"Your BMI is {bmi} you have normal weight")
+else:
+ print(f"Your BMI is {bmi} you are clinically obese.")
+
+  
+  
+        
+
 
 
 
